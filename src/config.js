656f1c7ab412a06d3b5c0a3f2350ac859ba419c5
lokiofinternet <jithinksajan@gmail.com>
2019-10-26 21:45:24 +0530

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "ap-south-1",
+      BUCKET: "notesapp-jithin-upload"
+    },
+    apiGateway: {
+      REGION: "ap-south-1",
+      URL: "https://pircg23i6b.execute-api.ap-south-1.amazonaws.com/prod"
+    },
+    cognito: {
+      REGION: "ap-south-1",
+      USER_POOL_ID: "ap-south-1_c8gC8WmPd",
+      APP_CLIENT_ID: "53j10krcbqdil4sb20oel095mh",
+      IDENTITY_POOL_ID: "YOUR_IDENTITY_POOL_ID"
+    }
+  };
